6d492657e94ff4a87be9abad34174579f374eab9
RedbeanGit <dubois.julien.mail@gmail.com>
2023-03-18 16:24:28 +0100

    feat: add remove route
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- src/main/java/fr/dopolytech/polyshop/cart/controllers/CartController.java
+++ src/main/java/fr/dopolytech/polyshop/cart/controllers/CartController.java
@@ -21,15 +21,28 @@ public class CartController {
 		this.purchaseOperations = purchaseOperations;
 	}
 
-	@PostMapping
+	@PostMapping("/add")
 	public Mono<Purchase> addToCart(@RequestBody AddToCartDto dto) {
-		return purchaseOperations.opsForValue().increment(dto.getProductId()).map(count -> new Purchase(dto.getProductId(), count));
+		return purchaseOperations.opsForValue().increment(dto.getProductId())
+				.map(count -> new Purchase(dto.getProductId(), count));
+	}
+
+	@PostMapping("/remove")
+	public Mono<Purchase> removeFromCart(@RequestBody AddToCartDto dto) {
+		if (!purchaseOperations.hasKey(dto.getProductId()).block()) {
+			return Mono.just(new Purchase(dto.getProductId(), 0L));
+		}
+		if (purchaseOperations.opsForValue().get(dto.getProductId()).block() <= 1) {
+			return purchaseOperations.delete(dto.getProductId()).map(count -> new Purchase(dto.getProductId(), 0L));
+		}
+		return purchaseOperations.opsForValue().decrement(dto.getProductId())
+				.map(count -> new Purchase(dto.getProductId(), count));
 	}
 
 	@GetMapping
 	public Flux<Purchase> findAll() {
 		return purchaseOperations
-			.keys("*")
-			.flatMap(key -> purchaseOperations.opsForValue().get(key).map(purchase -> new Purchase(key, purchase)));
+				.keys("*")
+				.flatMap(key -> purchaseOperations.opsForValue().get(key).map(purchase -> new Purchase(key, purchase)));
 	}
 }
