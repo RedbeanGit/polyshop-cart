4a69b8996a3c43a2d23b2d341bb41293b5f65777
RedbeanGit <dubois.julien.mail@gmail.com>
2023-04-29 19:07:21 +0200

    fix: missing bean annotation
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- src/main/java/fr/dopolytech/polyshop/cart/CartApplication.java
+++ src/main/java/fr/dopolytech/polyshop/cart/CartApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
@@ -13,6 +14,7 @@ public class CartApplication {
 		SpringApplication.run(CartApplication.class, args);
 	}
 
+	@Bean
 	@LoadBalanced
 	public WebClient.Builder loadBalancedWebClientBuilder() {
 		return WebClient.builder();
